b217e680e720177089a327a1e5baaad4ed326902
Edison Chee <edisonchee@live.com>
2016-05-30 14:43:51 +0800

    Implemented long polling for getUpdates

---
+++ src/telebot.js
@@ -0,0 +1,69 @@
+const EventEmitter = require('eventemitter3');
+const Request = require('request-promise');
+
+class Telebot extends EventEmitter {
+  constructor(token) {
+    super();
+    this._token = token;
+    this._offset = 0;
+  }
+
+  getMe() {
+    let options = {
+      uri: 'https://api.telegram.org/bot' + this._token + '/getUpdates',
+      resolveWithFullResponse: true
+    };
+
+    return Request(options);
+  }
+
+  getUpdates() {
+    let options = {
+      uri: 'https://api.telegram.org/bot' + this._token + '/getUpdates',
+      qs: {
+        offset: this._offset + 1,
+        timeout: 2000
+      },
+      resolveWithFullResponse: true,
+      forever: true
+    };
+
+    return Request(options).then(resp => {
+      if (resp.statusCode !== 200) {
+        throw new Error('resp.statusCode' + 'resp.body}');
+      };
+
+      let updates = JSON.parse(resp.body);
+
+      if (updates.ok) {
+        updates.result.forEach(update => {
+          this._offset = update.update_id;
+        });
+        return updates.result;
+      };
+    });
+  }
+
+  processUpdates() {
+    this.getUpdates().then(updates => {
+      updates.forEach(update => {
+        let message = update.message;
+        let callbackQuery = update.callbackQuery;
+        let inlineQuery = update.inline_query;
+        let chosenInlineResult = update.chosen_inline_result;
+
+        if (message) {
+          this.emit('message', message);
+        } else if (callbackQuery) {
+          this.emit('callback_query', callbackQuery);
+        } else if (inlineQuery) {
+          this.emit('inline_query', inlineQuery);
+        } else if (chosenInlineResult) {
+          this.emit('chosen_inline_result', chosenInlineResult);
+        }
+      });
+    });
+  }
+}
+
+module.exports = Telebot;
