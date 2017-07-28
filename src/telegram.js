776ed32d692b6c3f60cb6f87de6a76a416bd7dd1
Edison Chee <edisonchee@live.com>
2017-07-29 00:51:06 +0800

    Added addStickerToSet method

--- src/telegram.js
+++ src/telegram.js
@@ -360,6 +360,27 @@ const Telegram = EventEmitter => class extends EventEmitter {
     return this._request('createNewStickerSet', params, formData, callback);
   }
 
+  addStickerToSet(userId, name, pngFile, emojis, optionalParams, callback) {
+    let params = {
+      user_id: userId,
+      name: name,
+      png_sticker: pngFile,
+      emojis: emojis
+    };
+
+    let formData = {
+      png_sticker: pngFile
+    };
+
+    if (typeof optionalParams == 'function') {
+      callback = optionalParams;
+    } else {
+      Object.assign(params, optionalParams);
+    }
+
+    return this._request('addStickerToSet', params, formData, callback);
+  }
+
   sendVideo(chatId, video, optionalParams, callback) {
     let params = {
       chat_id: chatId,
