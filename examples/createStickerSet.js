956eeec3ee51076bbe23644bfd85bfe0ec90dfa5
edisonchee <edisonchee@live.com>
2020-04-20 13:16:23 +0800

    Update README with new example

--- examples/createStickerSet.js
+++ examples/createStickerSet.js
@@ -4,13 +4,13 @@ const fs = require('fs');
 
 // get your user_id by talking to your bot
 let userId = null;
-// [follow instructions from official docs](https://core.telegram.org/bots/api#createnewstickerset)
+// instructions: https://core.telegram.org/bots/api#createnewstickerset
 // Basic formula: 'name_digit_letters_underscores_only' + '_by_' + 'bot_username' (use @yourbotusername without the '@')
 let stickerSetName = 'sticker_set_name' + '_by_' + 'your_bot_username';
 // This is the title name that will appear on Telegram after you add the sticker set
 let stickerSetTitle = 'sticker set name on Telegram';
 // can be a local file, where you create a read stream object from, or use a file_id or HTTP URL
-// refer to [sendFile.js](sendFile.js) for usage pattern
+// refer to sendFile.js example for usage pattern
 let stickerFile = fs.createReadStream(__dirname + './bulb.png');
 // actual emoji to replace
 let emojis = "😊";
